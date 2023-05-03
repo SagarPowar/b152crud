877f0265a630a96fcfd9395432a445423f734187
HP <HP@192.168.1.89>
2023-05-03 18:33:30 +0530

    ggg

---
+++ 2Autowiring_setterbaseinjction_ByType/src/com/client/Test.java
@@ -0,0 +1,22 @@
+package com.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.model.Student;
+
+public class Test {
+public static void main(String[] args) {
+ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+Student s=(Student) ac.getBean("s");
+     System.out.println(s.getRollno());
+     System.out.println(s.getName());
+     System.out.println(s.getAdr().getAreaname());
+     System.out.println(s.getAdr().getCityname());
+
+}
+}
